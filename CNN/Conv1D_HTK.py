4e1a16e060cdb21dbba7e882663cddf429d23006
ariel-ferreira <ferreira.arl@outlook.com>
2022-06-21 00:35:42 -0300

    2022.06.20

--- CNN/Conv1D_HTK.py
+++ CNN/Conv1D_HTK.py
@@ -6,9 +6,11 @@ import re
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import load_model
-from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix
+from sklearn.metrics import (accuracy_score, f1_score, recall_score,
+                             precision_score, confusion_matrix)
 
-DATASET_ROOT = os.path.join(os.path.expanduser("~"), 'dataSet/audio/agender_distribution/')
+DATASET_ROOT = os.path.join(os.path.expanduser("~"),
+                            'dataSet/audio/agender_distribution/')
 VALID_SPLIT = 0.1
 SAMPLING_RATE = 8000
 SHUFFLE_SEED = 43
@@ -31,18 +33,6 @@ def path_to_audio(path):
     return audio
 
 
-def audio_to_fft(audio):
-    # Since tf.signal.fft applies FFT on the innermost dimension,
-    # we need to squeeze the dimensions and then expand them again
-    # after FFT
-    audio = tf.squeeze(audio, axis=-1)
-    fft = tf.signal.fft(tf.cast(tf.complex(real=audio, imag=tf.zeros_like(audio)), tf.complex64))
-    fft = tf.expand_dims(fft, axis=-1)
-    # Return the absolute value of the first half of the FFT
-    # which represents the positive frequencies
-    return tf.math.abs(fft[:, : (audio.shape[1] // 2), :])
-
-
 def path_to_mfc(path, dim):
     csv_file = tf.io.read_file(path, dim)
     tensor = tf.convert_to_tensor(csv_file)
@@ -51,7 +41,7 @@ def path_to_mfc(path, dim):
 
 def ReadCSV(file, dim):
     fin = file
-    fout = pd.read_csv(fin, dtype=np.float32, header=None, nrows = dim)
+    fout = pd.read_csv(fin, dtype=np.float32, header=None, nrows=dim)
     return fout
 
 
@@ -66,18 +56,12 @@ def HTK_paths_and_labels_to_dataset(audio_paths, labels, dim):
 
 
 # Read train files and split class from file
-train_file_list = pd.read_csv('7_class_train.csv')
+train_file_list = pd.read_csv(
+    '/home/arielferreira/github/Conv1D/CNN/file_lists/train_database_full.csv')
 train_audio_files = train_file_list['file']
 train_classes = train_file_list['class']
 train_audio_df = pd.DataFrame(train_audio_files)
 train_class_df = pd.DataFrame(train_classes)
-
-# Read test files and split class from file
-test_file_list = pd.read_csv('7_class_test.csv')
-test_audio_files = test_file_list['file']
-test_classes = test_file_list['class']
-test_audio_df = pd.DataFrame(test_audio_files)
-test_class_df = pd.DataFrame(test_classes)
 
 train_class_labels = list(train_classes.unique())
 print("Age categories identified: {}".format(train_class_labels,))
@@ -87,17 +71,30 @@ labels = []
 
 for label, category in enumerate(train_class_labels):
     print("Processing category {}".format(category,))
-    speaker_sample_paths = [os.path.join(DATASET_ROOT, train_audio_files[i]) for i in range(len(train_audio_files)) if train_classes[i] == category]
+    speaker_sample_paths = [os.path.join(DATASET_ROOT, train_audio_files[i])
+                            for i in range(len(train_audio_files))
+                            if train_classes[i] == category]
     audio_paths += speaker_sample_paths
     labels += [label] * len(speaker_sample_paths)
 
-print("Found {} files belonging to {} classes.".format(len(audio_paths), len(train_class_labels)))
+print("Found {} files belonging to {} classes.".format(len(audio_paths),
+      len(train_class_labels)))
+
+print(train_classes[:10])
+
+print(audio_paths[:10])
+print(labels[:-1])
+
+exit(0)
 
 # Shuffle
 rng = np.random.RandomState(SHUFFLE_SEED)
 rng.shuffle(audio_paths)
 rng = np.random.RandomState(SHUFFLE_SEED)
 rng.shuffle(labels)
+
+
+
 
 # Split into training and validation
 num_val_samples = int(VALID_SPLIT * len(audio_paths))
@@ -138,3 +135,21 @@ train_ds = HTK_paths_and_labels_to_dataset(train_audio_paths[:5], train_labels[:
 
 train_ds = train_ds.shuffle(buffer_size=BATCH_SIZE * 8, seed=SHUFFLE_SEED).batch(BATCH_SIZE)
 valid_ds = valid_ds.shuffle(buffer_size=32 * 8, seed=SHUFFLE_SEED).batch(32)
+
+
+
+
+
+
+
+
+# Read test files and split class from file
+test_file_list = pd.read_csv(
+    '/home/arielferreira/github/Conv1D/CNN/file_lists/test_database_full.csv')
+test_audio_files = test_file_list['file']
+test_classes = test_file_list['class']
+test_audio_df = pd.DataFrame(test_audio_files)
+test_class_df = pd.DataFrame(test_classes)
+
+# print(test_audio_df[:10])
+# print(test_class_df[:10])
