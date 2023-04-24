ae0034b322c8f2a7ed6b0118e3383dd8978609f1
Ariel Ferreira <78990249+ariel-ferreira@users.noreply.github.com>
2023-04-23 21:50:04 -0300

    Upload de um novo script

---
+++ 03-Python Scripts/String_manipulation_00.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Função do script: Ler arquivo txt com listas de arquivo (incluindo path) e trocar a segunda "/" por "-"
+# Autor: Ariel Ferreira
+# Data: 23/04/2023
+
+
+# In[1]:
+
+
+import re
+
+
+# In[2]:
+
+
+with open('TIMIT_all.scp', 'r') as f:
+    file = f.readlines()
+
+
+# In[50]:
+
+
+files = []
+for line in file:
+    line = re.sub('(?:[\/]*\/[^\/]*)(\/)', "\g<0>-", line)
+    line = re.sub('(.........)(/)', "\g<1>", line)
+    files.append(line)
+
+
+# In[54]:
+
+
+with open('TIMIT_all.scp', 'w') as txt:
+    for item in files:
+        txt.write(item)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
